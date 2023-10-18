2867ade5691d391d6fdca390dbec4cfd6d6f998f
rootdnh <rootdnh@gmail.com>
2023-10-18 17:24:48 -0300

    chore: changing the structure of projects section

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,10 +1,10 @@
 import banner from "./perfil.png";
 import theband from "./the-band.png";
 import circleEffect from "./new-assets/ball.png";
-import blog from "./blog-ts.png";
+import blog from "./blog.png";
 import zigzag from "./new-assets/zigzag.png";
 import blogApi from "./blog-api.png";
-import projectsPortifolio from "./portifolio.png";
+import projectsPortfolio from "./portfolio.png";
 import crudcli from "./crud-cli.png";
 import arrows from "./new-assets/arrows.png";
 import multidb from "./multi-db.png";
@@ -16,7 +16,7 @@ export {
   blog,
   zigzag,
   blogApi,
-  projectsPortifolio,
+  projectsPortfolio,
   crudcli,
   arrows,
   multidb
